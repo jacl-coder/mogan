fd2cae6bd2b9060a8e669f947a6edc8752df89e3
Da Shen <da@liii.pro>
2025-08-21 09:00:37 +0000

    更新 xmake/stem.lua 以适配 Liii STEM (#3)
    Reviewed-on: https://code.liiistem.cn/LiiiLabs/stem/pulls/3
    Co-authored-by: Da Shen <da@liii.pro>
    Co-committed-by: Da Shen <da@liii.pro>

--- xmake/stem.lua
+++ xmake/stem.lua
@@ -8,15 +8,15 @@
 -- It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 -- in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 
-stem_project_name = "Mogan STEM"
+stem_project_name = "Liii STEM"
 
-stem_homepage = "https://mogan.app"
+stem_homepage = "https://liiistem.cn"
 
-stem_binary_name = "MoganSTEM"
-stem_binary_linux = "moganstem"
+stem_binary_name = "LiiiSTEM"
+stem_binary_linux = "liiistem"
 stem_binary_macos = stem_binary_name
 stem_binary_windows = stem_binary_name .. ".exe"
 
-stem_lab_name = "moganlab"
-stem_lab_big_name= "MoganLab"
+stem_lab_name = "liiilabs"
+stem_lab_big_name= "LiiiLabs"
 stem_prefix_dir = "share/" .. stem_lab_name
